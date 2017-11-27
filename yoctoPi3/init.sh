7439806049330fa059d8babcab6d82dbbcf3fafa
drees81 <30475079+drees81@users.noreply.github.com>
2017-11-27 17:09:43 +0100

    Update init.sh

--- yoctoPi3/init.sh
+++ yoctoPi3/init.sh
@@ -8,6 +8,3 @@ BRANCH=rocko
 git clone -b rocko git://git.yoctoproject.org/poky
 git clone -b rocko git://git.openembedded.org/meta-openembedded
 git clone -b rocko git://git.yoctoproject.org/meta-raspberrypi
-
-cd ..
-source sources/poky/oe-init-build-env rpi-build
